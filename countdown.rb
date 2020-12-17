8242b4eb73c568f8295557016f392e8c9174b0b4
JC <bonassin@gmail.com>
2020-12-17 20:52:19 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,16 @@
-def countdown(time)
-  while num > 0
-
-  puts "#{num} SECOND(S)!"
-  num -= 1
-
+ef countdown(seconds)
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
-  "HAPPY NEW YEAR!"
+
+def countdown_with_sleep(seconds)
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
 end
